20bf53931fe2a9e0cfb20a5481691287d053d9c9
lucianalaura <luliviviani@gmail.com>
2023-03-28 18:49:35 +0200

    correcion

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-public class Main{
-    public static void main(String[] args) {
-        suma(2,3,4);
+public class Main {
+    public static void main(String args[]) {
+        suma(1,2,3);
 
-
+        //Crear un objeto miCoche en el main
+        coche micoche = new coche();
+        micoche.incrementaPuertas();
+        micoche.incrementaPuertas();
+        micoche.incrementaPuertas();
+        micoche.incrementaPuertas();
+        System.out.println(micoche.puertas);
     }
+    public static void suma(int a, int b, int c){
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);
+    }
+}
+
+//Crear una clase coche.
+class coche {
+    public int puertas = 4;
+    public void incrementaPuertas() {
+        this.puertas++;
+    }
+
 }
